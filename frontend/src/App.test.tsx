c1d094738e09702799192b703154ea43351423a7
Yasunori MAHATA <mahata777@gmail.com>
2023-10-25 17:30:43 +0900

    ✨ Add top page without actual contents

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,10 +1,26 @@
-import { expect, describe, it } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import App from './App'
+import { expect, describe, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { MemoryRouter } from "react-router-dom";
 
-describe('App', () => {
-    it("はじめての Vitest", () => {
-        render(<App />)
-        expect(screen.getByText("Click on the Vite and React logos to learn more")).toBeInTheDocument();
-    });
-})
+describe("App", () => {
+  it("shows 'login' link", () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText("Login")).toBeInTheDocument();
+  });
+
+  it("shows 'Posts' header", () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText("Posts")).toBeInTheDocument();
+  });
+});
