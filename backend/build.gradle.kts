0d441c555fce8efbd90aba8b1e9e17ca2e38e127
Yasunori MAHATA <mahata777@gmail.com>
2023-11-15 23:53:10 +0900

    ✨ Add GitHub OAuth login (#14)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -25,8 +25,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.20")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.3")
---
+++ backend/src/main/kotlin/org/mahata/ktlog/SecurityConfig.kt
@@ -0,0 +1,28 @@
+package org.mahata.ktlog
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests {
+                it.anyRequest().permitAll()
+            }
+            .oauth2Login {
+                it.defaultSuccessUrl("/")
+            }
+
+        return http.build()
+    }
+}
