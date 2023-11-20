d67173d450f845d315f8d4e1885af75d0994f1e1
Yasunori MAHATA <mahata777@gmail.com>
2023-11-20 22:20:18 +0900

    ✨ Introduce new features to fetch users' data

---
+++ frontend/src/repository/UsersRepository.ts
@@ -0,0 +1,22 @@
+export type User = {
+  name: string;
+  email: string;
+};
+
+export interface UsersRepository {
+  getMe(): Promise<User>;
+}
+
+export class NetworkUsersRepository implements UsersRepository {
+  async getMe(): Promise<User> {
+    const response = await fetch("/api/v1/users/me", {
+      method: "GET",
+    });
+    if (!response.ok) {
+      const text = await response.text();
+      throw new Error(`HTTP error! status: ${response.status}, body: ${text}`);
+    }
+
+    return response.json();
+  }
+}
