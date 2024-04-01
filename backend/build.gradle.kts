3d0ee48fd7836dc8eb43b5c124dd6d98f326b130
Yasunori MAHATA <mahata777@gmail.com>
2024-04-01 13:20:37 +0900

    💚 Make Flyway work for the latest version of Postgres

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.23")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
     implementation("org.flywaydb:flyway-core:10.10.0")
+    implementation("org.flywaydb:flyway-database-postgresql:10.10.0")
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
 
     runtimeOnly("org.postgresql:postgresql:42.7.2")
