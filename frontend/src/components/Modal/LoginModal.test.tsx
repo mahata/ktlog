887f023da7b8d211cf3b33df829520709394c4f1
Yasunori MAHATA <mahata777@gmail.com>
2024-06-06 11:44:48 +0900

    :sparkles: Add a feature to request access token and refresh token when logging in

--- frontend/src/components/Modal/LoginModal.test.tsx
+++ frontend/src/components/Modal/LoginModal.test.tsx
@@ -1,8 +1,23 @@
 import LoginModal from "./LoginModal";
 import { render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
+import {describe, it, expect, afterEach, vi, beforeEach} from "vitest";
+import userEvent from "@testing-library/user-event";
+
+const originalFetch = globalThis.fetch;
 
 describe("LoginModal", () => {
+
+  const mockedFetch = vi.fn();
+
+  beforeEach(() => {
+    mockedFetch.mockReset();
+    globalThis.fetch = mockedFetch;
+  });
+
+  afterEach(() => {
+    globalThis.fetch = originalFetch;
+  });
+
   it("restricts page scrolling when it's active", () => {
     expect(window.document.body.style.overflowY).not.toBe("hidden");
 
@@ -19,13 +34,40 @@ describe("LoginModal", () => {
     expect(window.document.body.style.overflowY).toBe("unset");
   });
 
-  it("shows username and password input fields", () => {
+  it("shows email and password input fields", () => {
     render(<LoginModal title="DOES NOT MATTER" />);
 
-    const usernameField = screen.getByLabelText("username")
-    expect(usernameField).toHaveAttribute("placeholder", "Please type your username");
+    const emailField = screen.getByLabelText("email")
+    expect(emailField).toHaveAttribute("placeholder", "Please type your email");
 
     const passwordField = screen.getByLabelText("password")
     expect(passwordField).toHaveAttribute("placeholder", "Please type your password");
   });
+
+  it("shows a login button", () => {
+    render(<LoginModal title="DOES NOT MATTER"/>);
+
+    expect(screen.getByRole("button", { name: "Send" }))
+  });
+
+  describe("Login Button", () => {
+    it("sends a request when clicked", async () => {
+      render(<LoginModal title="DOES NOT MATTER"/>);
+
+      await userEvent.type(screen.getByLabelText("email"), "my-email");
+      await userEvent.type(screen.getByLabelText("password"), "my-password");
+      await userEvent.click(screen.getByRole("button", { name: "Send" }));
+
+      expect(globalThis.fetch).toHaveBeenCalledWith(`/api/v1/auth`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "my-email",
+          password: "my-password",
+        }),
+      });
+    });
+  })
 });
--- frontend/src/components/Modal/LoginModal.tsx
+++ frontend/src/components/Modal/LoginModal.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 import { useAtom } from "jotai";
 import { loginModalAtom } from "./LoginModal.atoms";
 import { X } from "lucide-react";
@@ -8,7 +8,10 @@ type Props = {
 };
 
 export default function LoginModal({ title }: Props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [, setShowModal] = useAtom(loginModalAtom);
+
 
   useEffect(() => {
     document.body.style.overflowY = "hidden";
@@ -21,19 +24,54 @@ export default function LoginModal({ title }: Props) {
   return (
     <div className="fixed inset-0 z-10 flex items-center justify-center">
       <div className="absolute inset-0 z-10 bg-slate-400/40" />
-      <div className="z-20 w-96 rounded-xl bg-blue-50 p-4 shadow-2xl">
+      <div className="z-20 w-96 rounded bg-blue-50 p-4 shadow-2xl">
         <div className="flex justify-between">
           <div className="pb-3 pt-2 text-xl font-medium leading-5" aria-label="login-modal-title">{title}</div>
           <X size={24} onClick={() => setShowModal(false)}/>
         </div>
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-2">
           <div className="flex justify-between">
-            <label className="content-center" htmlFor="username">username</label>
-            <input className="rounded-l p-1" id="username" type="text" size={24} maxLength={256} placeholder="Please type your username"/>
+            <label className="content-center" htmlFor="email">email</label>
+            <input className="rounded p-1"
+                   id="email"
+                   type="text"
+                   value={email}
+                   size={24}
+                   maxLength={256}
+                   placeholder="Please type your email"
+                   onChange={(event) => setEmail(event.currentTarget.value)}
+            />
           </div>
           <div className="flex justify-between">
             <label className="content-center" htmlFor="password">password</label>
-            <input className="rounded-l p-1" id="password" type="text" size={24} maxLength={256} placeholder="Please type your password"/>
+            <input className="rounded p-1"
+                   id="password"
+                   type="text"
+                   value={password}
+                   size={24}
+                   maxLength={256}
+                   placeholder="Please type your password"
+                   onChange={(event) => setPassword(event.currentTarget.value)}
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+                className="rounded bg-blue-800 text-white py-1 px-1.5 shadow-2xl hover:bg-blue-700 hover:shadow-xl"
+                onClick={async () => {
+                  await fetch("/api/v1/auth", {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      email: email,
+                      password: password,
+                    })
+                  })
+                }}
+            >
+              Send
+            </button>
           </div>
         </div>
       </div>
