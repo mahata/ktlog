53c43bb5aebb671b3e8b5c574cef5e4e7fbd4b69
Yasunori MAHATA <mahata777@gmail.com>
2024-08-26 22:53:57 +0900

    :bug: Update the way to import library paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
 /// <reference types="vitest" />
 
+import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 	test: {
 		globals: true,
 		environment: "jsdom",
