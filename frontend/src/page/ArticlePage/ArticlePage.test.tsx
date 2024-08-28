d9539c4e7877539a6cec5a32ec7790803dd140aa
Yasunori MAHATA <mahata777@gmail.com>
2024-08-29 07:50:19 +0900

    :white_check_mark: Add Mocks to test ArticlePage

--- frontend/src/page/ArticlePage/ArticlePage.test.tsx
+++ frontend/src/page/ArticlePage/ArticlePage.test.tsx
@@ -1,6 +1,7 @@
+import { makeArticleFixture } from "@/fixture/makeArticleFixture";
+import { useArticleRepository } from "@/repository/useArticleRepository";
+import { _useArticleRepository } from "@/test-helper/__mocks__/useArticleRepository";
 import { render, screen } from "@testing-library/react";
-import { makeArticleFixture } from "../../fixture/makeArticleFixture";
-import { useArticleRepository } from "../../repository/useArticleRepository";
 import ArticlePage from "./ArticlePage";
 
 vi.mock("react-router-dom", async () => ({
@@ -10,19 +11,24 @@ vi.mock("react-router-dom", async () => ({
 		.mockReturnValue({ id: "00000000-0000-0000-0000-000000000000" }),
 }));
 
-vi.mock("../../repository/useArticleRepository");
+vi.mock(
+	"@/repository/useArticleRepository",
+	() => import("@/test-helper/__mocks__/useArticleRepository"),
+);
 
 describe("Article", () => {
+	const originalGetMock = _useArticleRepository.getMock;
+
+	afterEach(() => {
+		_useArticleRepository.getMock = originalGetMock;
+	});
+
 	it("shows an article", async () => {
 		const stubArticle = makeArticleFixture({
 			id: "00000000-0000-0000-0000-000000000000",
 		});
 
-		vi.mocked(useArticleRepository).mockReturnValue({
-			getAll: vi.fn().mockResolvedValue([]),
-			get: vi.fn().mockResolvedValue(stubArticle),
-		});
-
+		_useArticleRepository.getMock = vi.fn().mockResolvedValueOnce(stubArticle);
 		render(<ArticlePage />);
 
 		expect(await screen.findByText(stubArticle.title)).toBeVisible();
--- frontend/src/test-helper/__mocks__/useArticleRepository.ts
+++ frontend/src/test-helper/__mocks__/useArticleRepository.ts
@@ -2,14 +2,14 @@ import type { useArticleRepository } from "@/repository/useArticleRepository";
 import { getSampleArticleList } from "@/test-helper/stub";
 
 const testDoubles = {
-	get: vi.fn(() => Promise.resolve(getSampleArticleList()[0])),
+	getMock: vi.fn(() => Promise.resolve(getSampleArticleList()[0])),
 	getAllMock: vi.fn(() => Promise.resolve(getSampleArticleList())),
 };
 
 const useArticleRepositoryMock: () => ReturnType<typeof useArticleRepository> =
 	vi.fn(() => ({
 		getAll: testDoubles.getAllMock,
-		get: testDoubles.get,
+		get: testDoubles.getMock,
 	}));
 
 export {
