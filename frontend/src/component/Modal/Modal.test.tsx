02f682fe29908372f06d03653c14727c064a41d6
Yasunori MAHATA <mahata777@gmail.com>
2024-08-30 19:22:58 +0900

    :white_check_mark: Add more tests for Modal

--- frontend/src/component/Modal/Modal.test.tsx
+++ frontend/src/component/Modal/Modal.test.tsx
@@ -1,7 +1,19 @@
-import { Modal } from "@/component/Modal/Modal";
+import { Modal, type ModalRef } from "@/component/Modal/Modal";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { useRef } from "react";
 
 describe("Modal", () => {
+	const originalShowModal = HTMLDialogElement.prototype.showModal;
+
+	beforeEach(() => {
+		HTMLDialogElement.prototype.showModal = vi.fn();
+	});
+
+	afterEach(() => {
+		HTMLDialogElement.prototype.showModal = originalShowModal;
+	});
+
 	it("Modal is not visible by default", () => {
 		render(
 			<Modal>
@@ -12,4 +24,35 @@ describe("Modal", () => {
 		expect(screen.getByRole("dialog", { hidden: true })).toBeInTheDocument();
 		expect(screen.getByTestId("modal")).not.toBeVisible();
 	});
+
+	it("show modal", async () => {
+		render(<ParentComponent />);
+
+		await userEvent.click(screen.getByTestId("show-button"));
+
+		expect(HTMLDialogElement.prototype.showModal).toHaveBeenCalledTimes(1);
+	});
 });
+
+const ParentComponent = () => {
+	const dialogRef = useRef<ModalRef>(null);
+
+	return (
+		<>
+			<button
+				type="button"
+				data-testid="show-button"
+				onClick={() => dialogRef.current?.showModal()}
+			/>
+
+			<Modal ref={dialogRef}>
+				<button
+					type="button"
+					data-testid="close-button"
+					onClick={() => dialogRef.current?.close()}
+				/>
+				<div data-testid="modal" />
+			</Modal>
+		</>
+	);
+};
