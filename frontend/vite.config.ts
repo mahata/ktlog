546155fdf35c6b092b2ad91ea7810012e0d4981f
Yasunori MAHATA <mahata777@gmail.com>
2024-11-26 18:32:03 +0900

    :sparkles: Add feather icons

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 /// <reference types="vitest" />
 
-import path from "path";
+import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
